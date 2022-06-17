1002c3ae32fa4f4799f999f42a7f7804f300d582
畠中晃弘 <hatanaka@questetra.com>
2022-06-17 11:08:46 +0900

    Create workflow_apis.js

---
+++ workflow_apis.js
@@ -0,0 +1,13 @@
+const stopProcessInstance = async (username, password, pid) => {
+  const response = await fetch(`/API/OR/ProcessInstance/stop?processInstanceId=${pid}`, {
+    method: 'POST',
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(username + ':' + password)
+    })
+  });
+  return response.ok;
+};
+
+export default {
+  stopProcessInstance
+};
