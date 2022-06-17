da14893e6646447aab81c93125311440bef591eb
畠中晃弘 <hatanaka@questetra.com>
2022-06-17 11:35:45 +0900

    listProcessInstances

--- workflow_apis.js
+++ workflow_apis.js
@@ -9,6 +9,21 @@ const stopProcessInstance = async (username, password, pid) => {
   return response.ok;
 };
 
+const listProcessInstances = async (username, password, criteriaObj, start, limit) => {
+  const params = new URLSearchParams();
+  params.append('criteria', JSON.stringify(criteriaObj));
+  params.append('start', start);
+  params.append('limit', limit);
+  const response = await fetch(`/API/OR/ProcessInstance/list?${params.toString()}`, {
+    credentials: 'omit',
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(username + ':' + password)
+    })
+  });
+  return response.body;
+};
+
 export default {
-  stopProcessInstance
+  stopProcessInstance,
+  listProcessInstances
 };
